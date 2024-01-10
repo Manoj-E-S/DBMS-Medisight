3c9e309c3985f8cd48701266d7488e3dc5caf0c5
Kunal-JockL <kunalsalvak@gmail.com>
2024-01-10 11:55:39 +0530

    added delete

--- static/js/enterSymptoms.js
+++ static/js/enterSymptoms.js
@@ -4,3 +4,10 @@ $("#addNewSymptomBtn").click(() => {
     socket.emit('incrementSelectElementCount');
     window.location.reload();
 });
+
+$(".del-symptom-btn").click((event) => {
+    var buttonId = $(event.target).attr('id');
+    var symptomDivId = $(`#${buttonId}`).prev().attr("id");
+    $(`#${buttonId}`).remove();
+    $(`#${symptomDivId}`).remove(); 
+});
