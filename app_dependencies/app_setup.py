c3c08ec7ab172d77682759e2939a638afb8a75c2
Manoj E S <manoj.es.professional@gmail.com>
2024-01-15 00:08:57 +0530

    Add Flask-SQLAlchemy ORM related models

--- app_dependencies/app_setup.py
+++ app_dependencies/app_setup.py
@@ -3,10 +3,23 @@ import os
 from flask import Flask
 from flask_socketio import SocketIO
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import MetaData
+
 from dotenv import load_dotenv
-
-
 load_dotenv("../.env")
+
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention={
+        "ix": 'ix_%(column_0_label)s',
+        "uq": "uq_%(table_name)s_%(column_0_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s"
+    })
+
+
 
 def getHost():
     HOST = os.environ.get("DEV_HOST")
@@ -30,5 +43,5 @@ db_username, db_password, db_name = getDbCreds()
 app = Flask('app', static_folder='static', template_folder='templates')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql://{db_username}:{db_password}@{HOST}/{db_name}'
 
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, model_class=Base)
 socketio = SocketIO(app)
--- db_dependencies/models.py
+++ db_dependencies/models.py
@@ -1,9 +1,63 @@
+# Built-in Imports
+import os
+import sys
+
+# Third-party Imports:
+from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+# Custom Imports
+rootdir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+sys.path.append(rootdir)
 from app_dependencies import app_setup
 
 
 db = app_setup.db
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+class Department(db.Model):
+    department_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    department_name: Mapped[str] = mapped_column(String, nullable=False)
+
+    def __repr__(self):
+        return f'<Department {self.department_name}>'
+
+
+class Doctor(db.Model):
+    doctor_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    doctor_name: Mapped[str]  = mapped_column(String, nullable=False)
+    doctor_phno: Mapped[str]  = mapped_column(String)
+    doctor_office_no: Mapped[str]  = mapped_column(String)
+    doctor_office_address: Mapped[str]  = mapped_column(String)
+    department_id: Mapped[int] = mapped_column(ForeignKey(Department.department_id), nullable=False)
+
+    def __repr__(self):
+        return f'<Doctor {self.doctor_name} from Department-{self.department_id}>'
+
+
+class Test(db.Model):
+    test_id: Mapped[int] = mapped_column(Integer, nullable=False, primary_key=True)
+    test_name: Mapped[str] = mapped_column(String, nullable=False)
+    test_desc: Mapped[str] = mapped_column(String)
+
+    def __repr__(self):
+        return f'<Test {self.test_name}\n\t- {self.test_desc}>'
+
+
+class Symptom(db.Model):
+    symptom_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    symptom_name: Mapped[str] = mapped_column(String, nullable=False)
+    test_id: Mapped[int] = mapped_column(ForeignKey(Test.test_id), nullable=False)
+
+    def __repr__(self):
+        return f'<Symptom {self.symptom_name}>'
+    
+
+class Disease(db.Model):
+    disease_id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    disease_name: Mapped[str] = mapped_column(String, nullable=False)
+    disease_desc: Mapped[str] = mapped_column(String)
+    department_id: Mapped[int] = mapped_column(ForeignKey(Department.department_id), nullable=False)
+    symptom_id: Mapped[int] = mapped_column(ForeignKey(Symptom.symptom_id), nullable=False)
+
+    def __repr__(self):
+        return f'<Disease {self.disease_name}:\n\t- {self.disease_desc}>'
